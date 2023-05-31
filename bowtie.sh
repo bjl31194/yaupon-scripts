fd399f5f4fe0d5fefd9d2cce0b89ddf8bb0c732e
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2023-05-31 12:08:34 -0400

    Update bowtie.sh
    added slurm parameters

--- bowtie.sh
+++ bowtie.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
+## ABOUT THIS SCRIPT ##
+# Author: Ben Long (adapted from Shreena Pradhan)
+# Date : 05.31.2023
+# Description: aligns reads to a reference using bowtie
+# Run Information: This script is run manually.
+
+## SLURM PARAMETERS ##
+#SBATCH --job-name=indexing	                            # Job name
+#SBATCH --partition=batch		                            # Partition (queue) name
+#SBATCH --ntasks=1			                                # Single task job
+#SBATCH --cpus-per-task=4		                            # Number of cores per task
+#SBATCH --mem=10gb			                                # Total memory for job
+#SBATCH --time=8:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --output=/scratch/bjl31194/output/bowtie.%j.out			          # Standard output
+#SBATCH --error=/scratch/bjl31194/output/bowtie.%j.err                # Error log
+#SBATCH --mail-user=bjl31194@uga.edu                    # Where to send mail
+#SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
+
+
 cd /scratch/bjl31194/yaupon/trimmed_reads
 
 ml Bowtie2/2.4.1-GCC-8.3.0
