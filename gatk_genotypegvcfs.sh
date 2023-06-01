e38dbe3f003c8d7553980fcd4d8b6817cd567f5c
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2023-06-01 15:58:05 -0400

    Update gatk_genotypegvcfs.sh
    updated to pull from genomicsdb

--- gatk_genotypegvcfs.sh
+++ gatk_genotypegvcfs.sh
@@ -7,7 +7,7 @@
 # Run Information: This script is run manually.
 
 ## SLURM PARAMETERS ##
-#SBATCH --job-name=gatk_hapcaller                                 # Job name
+#SBATCH --job-name=gatk_genotypegvcfs                                 # Job name
 #SBATCH --partition=batch		                            # Partition (queue) name
 #SBATCH --ntasks=1			                                # Single task job
 #SBATCH --cpus-per-task=4		                            # Number of cores per task
@@ -19,11 +19,11 @@
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
 
-cd /scratch/bjl31194/yaupon/trimmed_reads
+cd /scratch/bjl31194/yaupon
 
 ml GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
 
-gatk --java-options "-Xmx10g" GenotypeGVCFs \
+gatk --java-options "-Xmx8g" GenotypeGVCFs \
    -R /scratch/bjl31194/yaupon/references/JYEU.hipmer.GA-F-4_assembly.fasta \
-   -V P2_A01.g.vcf.gz \
-   -O P2_A01_genotyped.vcf.gz
+   -V gendb://yaupon_db \
+   -O yaupon_cohort1_genotyped.vcf.gz
