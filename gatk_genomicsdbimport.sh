70b43a0c9461f9d3bbdc624de743e5f7dae14705
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2023-06-02 09:39:45 -0400

    Update gatk_genomicsdbimport.sh
    Gave more memory, set merge contigs option, and changed tmp dir to scratch to help with memory problems

--- gatk_genomicsdbimport.sh
+++ gatk_genomicsdbimport.sh
@@ -11,7 +11,7 @@
 #SBATCH --partition=batch		                            # Partition (queue) name
 #SBATCH --ntasks=1			                                # Single task job
 #SBATCH --cpus-per-task=4		                            # Number of cores per task
-#SBATCH --mem=16gb			                                # Total memory for job
+#SBATCH --mem=40gb			                                # Total memory for job
 #SBATCH --time=8:00:00  		                            # Time limit hrs:min:sec
 #SBATCH --output=/scratch/bjl31194/output/gatk_gdbi.%j.out			          # Standard output
 #SBATCH --error=/scratch/bjl31194/output/gatk_gdbi.%j.err                # Error log
@@ -23,15 +23,12 @@ cd /scratch/bjl31194/yaupon/trimmed_reads
 
 ml GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
 
-mkdir -p /lscratch/$SLURM_JOBID/tmp;
+mkdir -p /scratch/bjl31194/$SLURM_JOBID/tmp;
 
-gatk --java-options "-Xmx14g -Xms14g" \
+gatk --java-options "-Xmx32g -Xms32g" \
        GenomicsDBImport \
        --genomicsdb-workspace-path /scratch/bjl31194/yaupon/yaupon_db \
        -L JYEU.hipmer.GA-F-4_scaffolds.intervals.list \
        --sample-name-map cohort1.sample_map \
-       --tmp-dir /lscratch/$SLURM_JOBID/tmp \
-       --reader-threads 4 \
-       --merge-contigs-into-num-partitions 100
-
-cp -r /lscratch/${SLURM_JOB_ID} /scratch/bjl31194/output
+       --tmp-dir /scratch/bjl31194/$SLURM_JOBID/tmp \
+       --merge-contigs-into-num-partitions 10
