7dad0c28b785eb889140808fbb5f739e1fbb83f5
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2023-06-06 12:37:18 -0400

    trying to merge individual GVCFs the old fashioned way, screw GenomicsDB

---
+++ gatk_combinegvcfs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## ABOUT THIS SCRIPT ##
+# Author: Ben Long (adapted from Shreena Pradhan)
+# Date : 06.06.2023
+# Description: creates merged gvcf from per-sample gvcf files
+# Run Information: This script is run manually.
+
+## SLURM PARAMETERS ##
+#SBATCH --job-name=gatk_combinegvcfs                               # Job name
+#SBATCH --partition=batch		                            # Partition (queue) name
+#SBATCH --ntasks=1			                                # Single task job
+#SBATCH --cpus-per-task=4		                            # Number of cores per task
+#SBATCH --mem=64gb			                                # Total memory for job
+#SBATCH --time=24:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --output=/scratch/bjl31194/output/gatk_combinegvcfs.%j.out			          # Standard output
+#SBATCH --error=/scratch/bjl31194/output/gatk_combinegvcfs.%j.err                # Error log
+#SBATCH --mail-user=bjl31194@uga.edu                    # Where to send mail
+#SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
+
+
+cd /scratch/bjl31194/yaupon/trimmed_reads
+
+ml GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
+
+gatk CombineGVCFs \
+   -R /scratch/bjl31194/yaupon/references/JYEU.hipmer.GA-F-4_assembly.fasta \
+   --variant cohort1.sample_map \
+   -O yaupon_cohort1_combinedGVCF.g.vcf.gz
