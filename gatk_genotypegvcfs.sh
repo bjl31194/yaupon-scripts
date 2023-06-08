499e5248009fe8ae8d6953312cf0fa4b409363ed
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2023-06-08 10:28:24 -0400

    more memory bc genotyping is intensive

--- gatk_genotypegvcfs.sh
+++ gatk_genotypegvcfs.sh
@@ -11,8 +11,8 @@
 #SBATCH --partition=batch		                            # Partition (queue) name
 #SBATCH --ntasks=1			                                # Single task job
 #SBATCH --cpus-per-task=4		                            # Number of cores per task
-#SBATCH --mem=10gb			                                # Total memory for job
-#SBATCH --time=8:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --mem=56gb			                                # Total memory for job
+#SBATCH --time=72:00:00  		                            # Time limit hrs:min:sec
 #SBATCH --output=/scratch/bjl31194/output/gatk_genotypegvcfs.%j.out			          # Standard output
 #SBATCH --error=/scratch/bjl31194/output/gatk_genotypegvcfs.%j.err                # Error log
 #SBATCH --mail-user=bjl31194@uga.edu                    # Where to send mail
@@ -23,7 +23,7 @@ cd /scratch/bjl31194/yaupon
 
 ml GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
 
-gatk --java-options "-Xmx8g" GenotypeGVCFs \
+gatk --java-options "-Xmx64g" GenotypeGVCFs \
    -R /scratch/bjl31194/yaupon/references/JYEU.hipmer.GA-F-4_assembly.fasta \
    -V gendb://yaupon_db \
    -O yaupon_cohort1_genotyped.vcf.gz
