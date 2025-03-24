e180af90b61dca281d63fba91a5837752a12749c
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2025-03-24 15:54:33 -0400

    Create QCTrimming.sh
    Trying out trim_galore for trimming instead since trimmomatic did not remove adapters

---
+++ QCTrimming.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## ABOUT THIS SCRIPT ##
+# Author: Ben Long
+# Date : 3.24.2025
+# Description: This script runs basic QC and trimming scripts on a set of fastq files.
+
+## SLURM PARAMETERS ##
+#SBATCH --job-name=fastqc_Iv_plate1                            # Job name
+#SBATCH --partition=batch		                            # Partition (queue) name
+#SBATCH --ntasks=1			                                # Single task job
+#SBATCH --cpus-per-task=4		                            # Number of cores per task
+#SBATCH --mem=10gb			                                # Total memory for job
+#SBATCH --time=24:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --output=/scratch/bjl31194/log.%j			    # Standard output and error log
+#SBATCH --mail-user=bjl31194@uga.edu                    # Where to send mail
+#SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
+
+#set input and output directory variables
+OUTDIR="/scratch/bjl31194/yaupon/wgs/plate1/trimmed_reads"
+DATADIR="/scratch/bjl31194/yaupon/wgs/plate1/raw_reads"
+
+#if output directory doesn't exist, create it
+if [ ! -d $OUTDIR ]
+then
+    mkdir -p $OUTDIR
+fi
+
+#load modules
+ml FastQC/0.11.9-Java-11
+ml Trim_Galore/0.6.7-GCCcore-11.2.0
+ml cutadapt/4.9-GCCcore-12.3.0
+
+# run FastQC
+#fastqc /scratch/bjl31194/yaupon/wgs/plate1/raw_reads/*.fastq.gz --outdir $OUTDIR
+
+# trim Illumina adapters
+trim_galore --quality 20 --fastqc --illumina --retain_unpaired --paired $DATADIR/25055FL-01-01-01_S56_L006_R1_001.fastq.gz $DATADIR/25055FL-01-01-01_S56_L006_R2_001.fastq.gz
