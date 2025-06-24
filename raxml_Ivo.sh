ceb136d9530092c7a33d7a2f4b602528586523a0
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2025-06-24 14:14:34 -0400

    Update raxml_Ivo.sh
    added phylip construction

--- raxml_Ivo.sh
+++ raxml_Ivo.sh
@@ -3,29 +3,27 @@
 #SBATCH --partition=batch
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=64gb
-#SBATCH --time=3-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=/scratch/bjl31194/logs/%x_%j.out
 #SBATCH --error=/scratch/bjl31194/logs/%x_%j.error
 
-# name=$(awk "NR==${SLURM_ARRAY_TASK_ID}" /scratch/bjl31194/yaupon/wgs/plate1/read_array.txt)
-
-# command for making read array file:
-# ls -1 | sed 's/_L006_R.*//' | uniq > read_array.txt
-
-# set parameters
+# set variables
 DATADIR="/scratch/bjl31194/yaupon/wgs/plate1/vcf"
-
-PHYLIP="/scratch/bjl31194/yaupon/wgs/plate1/vcf/Ivom_plate1_filter.min4.phy"
+VCF="/scratch/bjl31194/yaupon/wgs/plate1/vcf/Ivom_plate1_filter_names.vcf.gz"
+PHYLIP="/scratch/bjl31194/yaupon/wgs/plate1/vcf/Ivom_plate1_filter_names.min4.phy"
 
 # load modules
 ml RAxML-NG/1.2.0-GCC-12.3.0
+ml Python/3.12.3-GCCcore-13.3.0
 
 # move to the vcf directory
 cd $DATADIR
 
+# build phylip matrix from vcf
+python vcf2phylip.py -i $VCF 
+
 # perform ML tree search and optimization
 raxml-ng --search1 --msa $PHYLIP --model GTR+G
-
