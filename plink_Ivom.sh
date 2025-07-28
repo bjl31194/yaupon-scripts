a4c44fcd2ae3e08573ce96c714969d3b30129b1c
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2025-07-28 12:01:36 -0400

    changed to run structure threader/raxml for full set

--- plink_Ivom.sh
+++ plink_Ivom.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=admix_Ilex
+#SBATCH --job-name=strct_th_Ilex
 #SBATCH --partition=batch
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=16gb
-#SBATCH --time=1-00:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64gb
+#SBATCH --time=9:30:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=/scratch/bjl31194/logs/%x_%j.out
 #SBATCH --error=/scratch/bjl31194/logs/%x_%j.error
@@ -16,17 +16,17 @@
 # ls -1 | sed 's/_L006_R.*//' | uniq > read_array.txt
 
 # set parameters
-DATADIR="/scratch/bjl31194/yaupon/wgs/plates1234/vcf"
+DATADIR="/scratch/bjl31194/yaupon/wgs/plates1234/vcf/structure"
 
 VCF="/scratch/bjl31194/yaupon/wgs/plates1234/vcf/Ilex_plates1234_filtered.vcf.gz"
 
 STRUCT_IN="/scratch/bjl31194/yaupon/wgs/plates1234/vcf/structure/Ilex384forStructure.recode.strct_in"
 
 # load modules
-ml PLINK/2.0.0-a.6.9-gfbf-2023b
-ml ADMIXTURE/1.3.0
-#ml Structure/2.3.4-GCC-11.3.0
-#ml structure_threader/1.3.10-foss-2022a
+#ml PLINK/2.0.0-a.6.9-gfbf-2023b
+#ml ADMIXTURE/1.3.0
+ml Structure/2.3.4-GCC-11.3.0
+ml structure_threader/1.3.10-foss-2022a
 
 # move to the proper directory
 cd $DATADIR
@@ -49,27 +49,27 @@ cd $DATADIR
 ## run ADMIXTURE ##
 
 # generate input files
-FILE=Ilex384
+#FILE=Ilex384
 
 # Generate the input file in plink format
-plink --vcf $VCF --double-id --make-bed --out $FILE --allow-extra-chr
+#plink --vcf $VCF --double-id --make-bed --out $FILE --allow-extra-chr
 
 # ADMIXTURE does not accept chromosome names that are not human chromosomes. We will thus just exchange the first column by 0
-awk '{$1="0";print $0}' $FILE.bim > $FILE.bim.tmp
-mv $FILE.bim.tmp $FILE.bim
+#awk '{$1="0";print $0}' $FILE.bim > $FILE.bim.tmp
+#mv $FILE.bim.tmp $FILE.bim
 
 # running ADMIXTURE for clusters size 3-8
-for i in {3..8}
-do
-    admixture --cv $FILE.bed $i > log${i}.out
-done
+#for i in {3..8}
+#do
+#    admixture --cv $FILE.bed $i > log${i}.out
+#done
 
 # yoink cross validation errors out of log files
-awk '/CV/ {print $3,$4}' *out | cut -c 4,7-20 > $FILE.cv.error
+#awk '/CV/ {print $3,$4}' *out | cut -c 4,7-20 > $FILE.cv.error
 
 ## STRUCTURE - for running on cluster ##
 
-#structure_threader run -K 5 -R 3 -i $STRUCT_IN -o $DATADIR -t 32 --ind indfile.csv -st /apps/eb/Structure/2.3.4-GCC-11.3.0/bin/structure
+structure_threader run -K 5 -R 3 -i $STRUCT_IN -o $DATADIR -t 32 --ind indfile.csv -st /apps/eb/Structure/2.3.4-GCC-11.3.0/bin/structure
 
 ## other misc scripts ##
 
