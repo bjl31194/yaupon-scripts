b81c8e3f7709699adcec797d7e749b7b18b53733
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2025-08-04 11:34:19 -0400

    fixed wallclock formatting

--- plink_Ivom.sh
+++ plink_Ivom.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=batch
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=64gb
-#SBATCH --time=7:00:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32gb
+#SBATCH --time=7-00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=/scratch/bjl31194/logs/%x_%j.out
 #SBATCH --error=/scratch/bjl31194/logs/%x_%j.error
