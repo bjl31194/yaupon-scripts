a0dc2543ae3113538230dfbe428ba9c048fa6d71
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2025-08-04 11:31:23 -0400

    Changed wallclock

--- plink_Ivom.sh
+++ plink_Ivom.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=64gb
-#SBATCH --time=9:30:00
+#SBATCH --time=7:00:00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=/scratch/bjl31194/logs/%x_%j.out
 #SBATCH --error=/scratch/bjl31194/logs/%x_%j.error
--- raxml_Ivo.sh
+++ raxml_Ivo.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=32gb
-#SBATCH --time=9:30:00
+#SBATCH --time=7:00:00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=/scratch/bjl31194/logs/%x_%j.out
 #SBATCH --error=/scratch/bjl31194/logs/%x_%j.error
