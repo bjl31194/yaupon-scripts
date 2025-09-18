696bf9d3d85c08032dc592a9dbaf32d13acb118d
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2025-09-18 15:47:22 -0400

    Update plink_Ivom.sh
    trying no thinning, also increasing max window threshold

--- plink_Ivom.sh
+++ plink_Ivom.sh
@@ -36,7 +36,7 @@ cd $DATADIR
 plink --vcf $VCF --double-id --allow-extra-chr \
 --set-missing-var-ids @:# \
 --maf 0.01 --geno 0.1 --mind 0.5 \
---thin 0.1 -r2 gz --ld-window 100 --ld-window-kb 1000 \
+-r2 gz --ld-window 100 --ld-window-kb 3000 \
 --ld-window-r2 0 \
 --out Ivom384
 
