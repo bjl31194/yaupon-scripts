3729480a2d7700cf01c836149c0dd8c509fb1930
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2025-10-06 14:56:41 -0400

    Made R script for calculating pairwise Fst on cluster

---
+++ pairwiseFst.R
@@ -0,0 +1,24 @@
+install.packages("adegenet", dep=TRUE)
+library(devtools)
+install_github("jgx65/hierfstat")
+library("adegenet")
+library("hierfstat")
+
+setwd("/scratch/bjl31194/yaupon/wgs/plates1234/vcf")
+
+Ivom384 <- read.structure("Ivom384forStructureRecode.STR", 
+                          n.ind=366,
+                          n.loc=36567, 
+                          onerowperind=TRUE,
+                          col.lab=1,
+                          col.pop=0,
+                          row.marknames=1,
+                          NA.char = "0",) #366 gts, 36567 markers 
+sites <- read.table("sites_Ivom384.txt")
+
+Ivom384@pop <- as.factor(sites$V1)
+
+#pairwise Fst
+pairwiseFst <- genet.dist(Ivom384, method = "WC84")
+
+write.csv(pairwiseFst, "./pairwiseFst_Ivom384.csv")
