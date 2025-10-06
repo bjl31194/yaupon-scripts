56fd69d57293ec0021b9c93da9a593d6113b0d56
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2025-10-06 15:01:03 -0400

    Create calc_pairwiseFst.sh

---
+++ calc_pairwiseFst.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=calc_pairwiseFst
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32gb
+#SBATCH --time=7-00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=/scratch/bjl31194/logs/%x_%j.out
+#SBATCH --error=/scratch/bjl31194/logs/%x_%j.error
+
+# set parameters
+DATADIR="/scratch/bjl31194/yaupon/wgs/plates1234/vcf"
+SCRIPTS="/home/bjl31194/yaupon/yaupon-scripts"
+ml R/4.4.2-gfbf-2024a
+
+cd $DATADIR
+
+R CMD BATCH $SCRIPTS/pairwiseFst.R
