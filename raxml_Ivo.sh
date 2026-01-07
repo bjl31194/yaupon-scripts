3900a37e6286f216c5f1c57bc9dcad60d74d7227
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2026-01-07 11:49:57 -0500

    Update raxml_Ivo.sh
    prepared to make ml tree from reduced representation dataset

--- raxml_Ivo.sh
+++ raxml_Ivo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=raxml_Ivo
+#SBATCH --job-name=raxml_Ilex
 #SBATCH --partition=batch
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -15,19 +15,19 @@
 #bcftools view -Ou Ivom_plate1_filter.vcf.gz | bcftools reheader -s names_plate1.txt -o Ivom_plate1_filter_names.vcf.gz
 
 # set variables
-DATADIR="/scratch/bjl31194/yaupon/wgs/plates1234/vcf"
-VCF="/scratch/bjl31194/yaupon/wgs/plates1234/vcf/Ivom_only_384_filtered_names.vcf.gz"
-PHYLIP="/scratch/bjl31194/yaupon/wgs/plates1234/vcf/Ivom384.min4.phy"
+OUTDIR="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew/phylo"
+VCF="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew/Ilex_redrep_filter.vcf.gz"
+PHYLIP="/scratch/bjl31194/yaupon/wgs/plates1234/vcf/Ilex_redrep.min4.phy"
 
 # load modules
 ml RAxML-NG/1.2.2-GCC-13.2.0
 ml Python/3.12.3-GCCcore-13.3.0
 
 # move to the vcf directory
-cd $DATADIR
+cd $OUTDIR
 
 # build phylip matrix from vcf
-python /home/bjl31194/yaupon/yaupon-scripts/vcf2phylip.py -i $VCF --output-folder $DATADIR --output-prefix Ivom384
+python /home/bjl31194/yaupon/yaupon-scripts/vcf2phylip.py -i $VCF --output-folder $OUTDIR --output-prefix Ilex_redrep
 
 # perform ML tree search and optimization
-raxml-ng --all --bs-trees 100 --msa $PHYLIP --model GTR+G
+raxml-ng --all --bs-trees 1000 --msa $PHYLIP --model GTR+G
