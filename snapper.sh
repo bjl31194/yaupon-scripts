e380c203c92ce363a15149418ff531bfdceadf14
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2026-02-25 13:09:24 -0500

    Create snapper.sh

---
+++ snapper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=snapper
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32gb
+#SBATCH --time=3-00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=/scratch/bjl31194/logs/%x_%j.out
+#SBATCH --error=/scratch/bjl31194/logs/%x_%j.error
+  
+  # Set up job environment.
+  set -o errexit  # Exit the script on any error
+  set -o nounset  # Treat any unset variables as an error
+  module --quiet purge  # Reset the modules to the system default
+  
+  # Load the beast2 module.
+  module load Beast/2.7.0-GCC-11.3.0-CUDA-11.7.0
+
+# set parameters
+DATADIR="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew/phylo"
+
+cd $DATADIR
+
+  # Run snapper
+  beast -threads 16 snapper.xml
