735287f88a2adc21668ed56f82715b999a6a6685
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2026-03-10 15:29:26 -0400

    Update alphafold.sh

--- alphafold.sh
+++ alphafold.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=alphafold
-#SBATCH --partition=batch
 #SBATCH --partition=gpu_p
-#SBATCH --gres=gpu:L4:1
-#SBATCH --nodes=1
+#SBATCH --gres=gpu:A100:1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=700gb
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=40gb
 #SBATCH --time=7-00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=/scratch/bjl31194/logs/%x_%j.out
@@ -16,7 +14,8 @@ OUTDIR="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew/alphafold"
 
 cd $OUTDIR
 
-ml CUDA/12.1.1
 ml AlphaFold/2.3.2-foss-2023a-CUDA-12.1.1
 
-alphafold --fasta_paths ./AL-CW-6_ADH.fasta --output_dir . --max_template_date 3000-01-01
+export ALPHAFOLD_DATA_DIR=/db/AlphaFold/2.3.2
+
+alphafold --data_dir /db/AlphaFold/2.3.1 --output_dir . --model_names model_1 --fasta_paths ./AL-CW-6_ADH.fasta --max_template_date 2026-03-10
