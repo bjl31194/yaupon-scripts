f6f7e91fe425438caf94653fa339947e8d4df565
bjl31194 <89357533+bjl31194@users.noreply.github.com>
2026-03-20 11:29:59 -0400

    added script to run mkado

--- degenotate.sh
+++ degenotate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=degenotate_singlecopy
+#SBATCH --job-name=mkado
 #SBATCH --partition=batch
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=40gb
 #SBATCH --time=3-00:00
 #SBATCH --mail-type=END,FAIL
@@ -11,7 +11,7 @@
 #SBATCH --error=/scratch/bjl31194/logs/%x_%j.error
 
 # set parameters
-OUTDIR="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew/degenotate/single_copy"
+OUTDIR="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew/mkado"
 if [ ! -d $OUTDIR ]
 then
     mkdir -p $OUTDIR
@@ -20,21 +20,27 @@ fi
 DATADIR="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew"
 
 REF="/scratch/bjl31194/yaupon/references/Ilex_vomitoria_var_GA_F_4_HAP1_V1_release/Ilex_vomitoria_var_GA_F_4/sequences/Ilex_vomitoria_var_GA_F_4.HAP1.mainGenome.fasta"
-GFF="/scratch/bjl31194/yaupon/wgs/plates1-5/ann/Ivo_Ipa_singlecopy.gff3"
-VCF="Ilex1-5_names_filter.vcf.gz"
+GFF="/scratch/bjl31194/yaupon/wgs/plates1-5/ann/Ilex_Hap1.filter.gff3"
+VCF="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew/Ivom1-5_names_newfilter.vcf.gz"
+OUT="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew/MC-IP-2.vcf.gz"
 MAF=0.1
+GENES="/scratch/bjl31194/yaupon/wgs/plates1-5/vcfnew/gene_IDs.txt"
 
 
-cd $DATADIR
+cd $OUTDIR
 
 # index vcf
-ml BCFtools/1.21-GCC-13.3.0
-bcftools index -t $VCF --threads 8 
+#ml BCFtools/1.21-GCC-13.3.0
+#bcftools index -t $VCF --threads 8 
 
 conda init
 
 # load conda environment
-conda activate degenotate
+# conda activate degenotate
+conda activate mkado
 
 # run degenotate
-/home/bjl31194/.conda/envs/degenotate/bin/degenotate.py -a ${GFF} -g ${REF} -v ${VCF} -maf ${MAF} -e exclude.txt -u outgroup_Ipa.txt -o ${OUTDIR} -sfs --overwrite
+# /home/bjl31194/.conda/envs/degenotate/bin/degenotate.py -a ${GFF} -g ${REF} -v ${VCF} -maf ${MAF} -e exclude.txt -u outgroup_Ipa.txt -o ${OUTDIR} -sfs --overwrite
+
+# run mkado
+mkado vcf --vcf ${VCF} --outgroup-vcf ${OUT} --ref ${REF} --gff ${GFF} --per-gene --workers 8 --format tsv --verbose -a 
